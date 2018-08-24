559eddcf83432975b0f5b4b1a8cda3d6362f34c9
Admin GeauxCommerce <admin@geauxcommerce.org>
2018-08-24 16:02:19 -0500

    Writing to Stdout

--- app.go
+++ app.go
@@ -9,6 +9,18 @@ import (
 	"os"
 )
 
+func init() {
+	// Log as JSON instead of the default ASCII formatter.
+	log.SetFormatter(&log.JSONFormatter{})
+
+	// Output to stdout instead of the default stderr
+	// Can be any io.Writer, see below for File example
+	log.SetOutput(os.Stdout)
+
+	// Only log the warning severity or above.
+	log.SetLevel(log.InfoLevel)
+}
+
 func main() {
 	// Register handlers
 	r := mux.NewRouter()
@@ -16,6 +28,12 @@ func main() {
 
 	s.Methods("GET").Path("/product/{id:[0-9]+}").
 		HandlerFunc(GetProduct)
+
+	s.Methods("GET").Path("/debug").
+		HandlerFunc(LogDebug)
+
+	s.Methods("GET").Path("/warning").
+		HandlerFunc(LogWarning)
 
 	s.Methods("GET").Path("/error").
 		HandlerFunc(LogError)
@@ -39,10 +57,24 @@ func GetProduct(w http.ResponseWriter, r *http.Request) {
 		log.Errorf("id not included: %v\n", productId)
 	} else {
 		p := `{"productId":` + productId + `}`
-		log.Infof("productId: %v\n", productId)
+		log.Infof("productId: %v", productId)
 		w.Header().Set("Content-Type", "application/json; charset=utf-8")
 		fmt.Fprintf(w, p)
 	}
+}
+
+func LogDebug(w http.ResponseWriter, r *http.Request) {
+	log.Debug("Log /debug URL invoked. Test debug log")
+	c := `{"DebugPage": "This is a debug"}`
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	fmt.Fprintf(w, c)
+}
+
+func LogWarning(w http.ResponseWriter, r *http.Request) {
+	log.Warn("Log /warning URL invoked. Test warning log")
+	c := `{"WarningPage": "This is a warning"}`
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	fmt.Fprintf(w, c)
 }
 
 func LogError(w http.ResponseWriter, r *http.Request) {
